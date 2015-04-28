39851bb65dd3932c1dff4fe18bf17fd1eb5b05a6
Art4 <art4@wlabs.de>
2015-04-28 22:46:11 +0200

    Unittests for Emojione\Emojione class

---
+++ lib/php/test/EmojioneTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Emojione\Test;
+
+use Emojione\Emojione;
+
+class EmojioneTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * test Emojione::toImage()
+     *
+     * @return void
+     */
+    public function testToImage()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! <img class="emojione" alt="😄" src="//cdn.jsdelivr.net/emojione/assets/png/1F604.png?v=1.2.4"/> <img class="emojione" alt="&#x1f604;" src="//cdn.jsdelivr.net/emojione/assets/png/1F604.png?v=1.2.4"/>';
+
+        $this->assertEquals(Emojione::toImage($test), $expected);
+    }
+
+    /**
+     * test Emojione::unifyUnicode()
+     *
+     * @return void
+     */
+    public function testUnifyUnicode()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! 😄 😄';
+
+        $this->assertEquals(Emojione::unifyUnicode($test), $expected);
+    }
+
+    /**
+     * test Emojione::shortnameToUnicode()
+     *
+     * @return void
+     */
+    public function testShortnameToUnicode()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! 😄 😄';
+
+        $this->assertEquals(Emojione::shortnameToUnicode($test), $expected);
+    }
+
+
+    /**
+     * test Emojione::shortnameToAscii()
+     *
+     * @return void
+     */
+    public function testShortnameToAscii()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! 😄 :]';
+
+        $this->assertEquals(Emojione::shortnameToAscii($test), $expected);
+    }
+
+    /**
+     * test Emojione::shortnameToImage()
+     *
+     * @return void
+     */
+    public function testShortnameToImage()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! 😄 <img class="emojione" alt="&#x1f604;" src="//cdn.jsdelivr.net/emojione/assets/png/1F604.png?v=1.2.4"/>';
+
+        $this->assertEquals(Emojione::shortnameToImage($test), $expected);
+    }
+
+    /**
+     * test Emojione::toShort()
+     *
+     * @return void
+     */
+    public function testToShort()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! :smile: :smile:';
+
+        $this->assertEquals(Emojione::toShort($test), $expected);
+    }
+
+    /**
+     * test Emojione::unicodeToImage()
+     *
+     * @return void
+     */
+    public function testUnicodeToImage()
+    {
+        $test     = 'Hello world! 😄 :smile:';
+        $expected = 'Hello world! <img class="emojione" alt="😄" src="//cdn.jsdelivr.net/emojione/assets/png/1F604.png?v=1.2.4"/> :smile:';
+
+        $this->assertEquals(Emojione::unicodeToImage($test), $expected);
+    }
+}
