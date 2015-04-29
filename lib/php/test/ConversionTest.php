25bb31610e863975477a1690cc45dad7087a924f
Art4 <art4@wlabs.de>
2015-04-29 23:14:26 +0200

    Added tests for unicode and shortcode conversion

--- lib/php/test/ConversionTest.php
+++ lib/php/test/ConversionTest.php
@@ -31,7 +31,7 @@ class ConversionTest extends \PHPUnit_Framework_TestCase
      *
      * @return void
      */
-    public function testShortnameMidSentence()
+    public function testShortnameInsideSentence()
     {
         $unicode   = 'The 🐌 is Emoji One\'s official mascot.';
         $shortname = 'The :snail: is Emoji One\'s official mascot.';
@@ -44,6 +44,163 @@ class ConversionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
         $this->assertEquals(Emojione::toImage($unicode), $image);
         $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname mid sentence with a comma
+     *
+     * @return void
+     */
+    public function testShortnameInsideSentenceWithComma()
+    {
+        $unicode   = 'The 🐌, is Emoji One\'s official mascot.';
+        $shortname = 'The :snail:, is Emoji One\'s official mascot.';
+        $image     = 'The <img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>, is Emoji One\'s official mascot.';
+        $image_fix = 'The <img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>, is Emoji One\'s official mascot.';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname at start of sentence
+     *
+     * @return void
+     */
+    public function testShortnameAtStartOfSentence()
+    {
+        $unicode   = '🐌 mail.';
+        $shortname = ':snail: mail.';
+        $image     = '<img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/> mail.';
+        $image_fix = '<img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/> mail.';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname at start of sentence with apostrophe
+     *
+     * @return void
+     */
+    public function testShortnameAtStartOfSentenceWithApostrophe()
+    {
+        $unicode   = '🐌\'s are cool!';
+        $shortname = ':snail:\'s are cool!';
+        $image     = '<img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>\'s are cool!';
+        $image_fix = '<img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>\'s are cool!';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname at end of sentence
+     *
+     * @return void
+     */
+    public function testShortnameAtEndOfSentence()
+    {
+        $unicode   = 'Emoji One\'s official mascot is 🐌.';
+        $shortname = 'Emoji One\'s official mascot is :snail:.';
+        $image     = 'Emoji One\'s official mascot is <img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>.';
+        $image_fix = 'Emoji One\'s official mascot is <img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>.';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname at end of sentence with alternate punctuation
+     *
+     * @return void
+     */
+    public function testShortnameAtEndOfSentenceWithAlternatePunctuation()
+    {
+        $unicode   = 'Emoji One\'s official mascot is 🐌!';
+        $shortname = 'Emoji One\'s official mascot is :snail:!';
+        $image     = 'Emoji One\'s official mascot is <img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>!';
+        $image_fix = 'Emoji One\'s official mascot is <img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>!';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * test shortname at end of sentence with preceeding colon
+     *
+     * @return void
+     */
+    public function testShortnameAtEndOfSentenceWithPreceedingColon()
+    {
+        $unicode   = 'Emoji One\'s official mascot: 🐌';
+        $shortname = 'Emoji One\'s official mascot: :snail:';
+        $image     = 'Emoji One\'s official mascot: <img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>';
+        $image_fix = 'Emoji One\'s official mascot: <img class="emojione" alt="&#x1f40c;" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png?v=1.2.4"/>';
+
+        $this->assertEquals(Emojione::toShort($unicode), $shortname);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $image_fix);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $unicode);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($unicode), $image);
+        $this->assertEquals(Emojione::toImage($shortname), $image_fix);
+    }
+
+    /**
+     * shortname inside of IMG tag
+     *
+     * @return void
+     */
+    public function testShortnameInsideOfImgTag()
+    {
+        $unicode   = 'The <img class="emojione" alt="🐌" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png" /> is Emoji One\'s official mascot.';
+        $shortname = 'The <img class="emojione" alt=":snail:" src="//cdn.jsdelivr.net/emojione/assets/png/1F40C.png" /> is Emoji One\'s official mascot.';
+
+        $this->assertEquals(Emojione::toShort($unicode), $unicode);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $shortname);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $shortname);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $unicode);
+        $this->assertEquals(Emojione::toImage($unicode), $unicode);
+        $this->assertEquals(Emojione::toImage($shortname), $shortname);
+    }
+
+    /**
+     * # characters inside of OBJECT tag
+The <object class="emojione" data="//cdn.jsdelivr.net/emojione/assets/svg/1F40C.svg" type="image/svg+xml" standby="🐌">🐌</object> is Emoji One's official mascot.
+     *
+     * @return void
+     */
+    public function testShortnameInsideOfObjectTag()
+    {
+        $unicode   = 'The <object class="emojione" data="//cdn.jsdelivr.net/emojione/assets/svg/1F40C.svg" type="image/svg+xml" standby="🐌">🐌</object> is Emoji One\'s official mascot';
+        $shortname = 'The <object class="emojione" data="//cdn.jsdelivr.net/emojione/assets/svg/1F40C.svg" type="image/svg+xml" standby=":snail:">:snail:</object> is Emoji One\'s official mascot';
+
+        $this->assertEquals(Emojione::toShort($unicode), $unicode);
+        $this->assertEquals(Emojione::shortnameToImage($shortname), $shortname);
+        $this->assertEquals(Emojione::shortnameToUnicode($shortname), $shortname);
+        $this->assertEquals(Emojione::unicodeToImage($unicode), $unicode);
+        $this->assertEquals(Emojione::toImage($unicode), $unicode);
+        $this->assertEquals(Emojione::toImage($shortname), $shortname);
     }
 
     /**
