b891b7544e0ad701c004e236038b081105b5012a
Marcelo Schmidt <marcelo.schmidt@konecty.com>
2015-06-09 23:55:38 -0300

    Adds official emojione:emojione meteor package

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'emojione:emojione',
+  summary: 'Meteor Package of http://www.emojione.com/ set',
+  version: '1.4.0',
+  git: 'https://github.com/Ranks/emojione.git',
+  name: "emojione:emojione"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.addFiles('assets/css/emojione.css', 'client');
+
+  api.use('templating', 'client');
+  api.addFiles('lib/js/emojione.js', ['client', 'server']);
+  api.addFiles('lib/meteor/emojione-client.js', ['client']);
+});
