885a20471819746aa18d4da7b021dcd6f90d6443
Matthias Burtscher <matthias.burtscher@fusonic.net>
2015-07-03 12:58:50 +0200

    Update Client.php

--- lib/php/src/Client.php
+++ lib/php/src/Client.php
@@ -81,7 +81,7 @@ class Client implements ClientInterface
             $ruleset = $this->getRuleset();
             $asciiRegexp = $ruleset->getAsciiRegexp();
 
-            $string = preg_replace_callback('/'.$this->ignoredRegexp.'|((\\s|'.hex2bin('c2a0').'|^)'.$asciiRegexp.'(?=\\s|'.hex2bin('c2a0').'|$|[!,\.]))/S', array($this, 'asciiToUnicodeCallback'), $string);
+            $string = preg_replace_callback('/'.$this->ignoredRegexp.'|((\\s|'.\hex2bin('c2a0').'|^)'.$asciiRegexp.'(?=\\s|'.\hex2bin('c2a0').'|$|[!,\.]))/S', array($this, 'asciiToUnicodeCallback'), $string);
         }
 
         return $string;
@@ -117,7 +117,7 @@ class Client implements ClientInterface
             $ruleset = $this->getRuleset();
             $asciiRegexp = $ruleset->getAsciiRegexp();
 
-            $string = preg_replace_callback('/'.$this->ignoredRegexp.'|((\\s|'.hex2bin('c2a0').'|^)'.$asciiRegexp.'(?=\\s|'.hex2bin('c2a0').'|$|[!,\.]))/S', array($this, 'asciiToImageCallback'), $string);
+            $string = preg_replace_callback('/'.$this->ignoredRegexp.'|((\\s|'.\hex2bin('c2a0').'|^)'.$asciiRegexp.'(?=\\s|'.\hex2bin('c2a0').'|$|[!,\.]))/S', array($this, 'asciiToImageCallback'), $string);
         }
 
         return $string;
