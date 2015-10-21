858d128b57458740a4253471de6d93cff1eedcf6
Kaviraj <kavirajkanagaraj@gmail.com>
2015-10-21 13:06:39 +0530

    adding default unicode support for python2.7

--- lib/python/emojipy/tests/test_conversion.py
+++ lib/python/emojipy/tests/test_conversion.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8; -*-
 
+from __future__ import unicode_literals
 from unittest import TestCase
 from emojipy import Emoji
 
--- lib/python/emojipy/tests/test_ruleset.py
+++ lib/python/emojipy/tests/test_ruleset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; -*-
 
+from __future__ import unicode_literals
 from unittest import TestCase
 import json
 
