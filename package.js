62d474b719057726356fb02e10cf4a273e7eeed2
Gabriel Engel <gabriel.engel@gmail.com>
2015-12-18 10:59:51 -0200

    Bump version of the Meteor package to 2.0.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'emojione:emojione',
   summary: 'Meteor Package of http://www.emojione.com/ set',
-  version: '1.5.2',
+  version: '2.0.0',
   git: 'https://github.com/Ranks/emojione.git'
 });
 
