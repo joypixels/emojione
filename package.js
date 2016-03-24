a57928a99648bcd60f6c5f22d9d0c4a722276dcb
Diego Sampaio <chinello@gmail.com>
2016-03-24 10:30:16 -0300

    Bump Meteor package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'emojione:emojione',
   summary: 'Meteor Package of http://www.emojione.com/ set',
-  version: '2.1.2',
+  version: '2.1.2_1',
   git: 'https://github.com/Ranks/emojione.git'
 });
 
