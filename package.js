669caa73ba2851515c0995078960690d00b2678c
caseyahenson <caseyh@bellwethercorp.com>
2016-06-10 16:56:59 -0500

    v2.2.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'emojione:emojione',
   summary: 'Meteor Package of http://www.emojione.com/ set',
-  version: '2.2.1',
+  version: '2.2.2',
   git: 'https://github.com/Ranks/emojione.git'
 });
 
