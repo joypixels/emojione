d5c9c09d0da8a88c22c79085f4a439d8c9e5e88d
Rafael Kellermann Streit <rafaelks@me.com>
2016-10-10 10:07:43 -0300

    Add support to Swift as a struct object

---
+++ lib/swift/README.md
@@ -0,0 +1,14 @@
+# Emojione utility for Swift
+
+
+### How to use
+
+This utility provides a method to convert from shortname to unicode characters.
+
+### How to re-generate mapping
+
+```
+cd lib/ios/generator
+npm install
+node generate.js
+```
---
+++ lib/swift/generator/Emojione.swift
@@ -0,0 +1,34 @@
+//
+//  Emojione.swift
+//
+//  Created by Rafael Kellermann Streit (@rafaelks) on 10/10/16.
+//  Copyright (c) 2016.
+//
+
+import Foundation
+
+struct Emojione {
+    static let values = [
+        <%= mapping %>
+    ]
+    
+    static func transform(string: String) -> String {
+        let oldString = string as NSString
+        var transformedString = string as NSString
+        
+        let regex = try! NSRegularExpression(pattern: ":([-+\\w]+):", options: [])
+        let matches = regex.matches(in: string, options: [], range: NSMakeRange(0, oldString.length))
+        print(matches.count)
+        
+        for result in matches {
+            guard result.numberOfRanges == 2 else { continue }
+            
+            let shortname = oldString.substring(with: result.rangeAt(1))
+            if let emoji = values[shortname] {
+                transformedString = transformedString.replacingCharacters(in: result.rangeAt(0), with: emoji) as NSString
+            }
+        }
+        
+        return transformedString as String
+    }
+}
