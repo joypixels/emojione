9296bb46432c171e6a98188e9fc69f32dce006e9
Ranil Wijeyratne <me@ranil.ch>
2016-11-06 23:28:18 +0100

    Wrap emojis with custom font

---
+++ lib/android/com/emojione/CustomTypeFaceSpan.java
@@ -0,0 +1,48 @@
+package com.emojione;
+
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextPaint;
+import android.text.style.TypefaceSpan;
+
+public class CustomTypeFaceSpan extends TypefaceSpan {
+
+    private final Typeface newType;
+
+    public CustomTypeFaceSpan(Typeface type) {
+        // just a placeholder. family is not used:
+        super("serif");
+        newType = type;
+    }
+
+    @Override
+    public void updateDrawState(TextPaint ds) {
+        applyCustomTypeFace(ds, newType);
+    }
+
+    @Override
+    public void updateMeasureState(TextPaint paint) {
+        applyCustomTypeFace(paint, newType);
+    }
+
+    private static void applyCustomTypeFace(Paint paint, Typeface tf) {
+        int oldStyle;
+        Typeface old = paint.getTypeface();
+        if (old == null) {
+            oldStyle = 0;
+        } else {
+            oldStyle = old.getStyle();
+        }
+
+        int fake = oldStyle & ~tf.getStyle();
+        if ((fake & Typeface.BOLD) != 0) {
+            paint.setFakeBoldText(true);
+        }
+
+        if ((fake & Typeface.ITALIC) != 0) {
+            paint.setTextSkewX(-0.25f);
+        }
+
+        paint.setTypeface(tf);
+    }
+}
--- lib/android/generator/Emojione.java
+++ lib/android/generator/Emojione.java
@@ -1,33 +1,41 @@
 package com.emojione;
 
+import android.graphics.Typeface;
 import android.os.Build;
+import android.support.v4.util.Pair;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public abstract class Emojione
-{
-    private static final HashMap<String, String> _shortNameToUnicode = new HashMap<String, String>();
+public class Emojione {
+
+    private static final Map<String, Pair<Integer[], Integer>> SHORT_NAME_TO_UNICODE = new HashMap<>();
+    private static final Set<Integer> UNICODES = new HashSet<>();
     private static final Pattern SHORTNAME_PATTERN = Pattern.compile(":([-+\\w]+):");
 
+    private Emojione() {
+    }
+
     /**
-     * Replace shortnames to unicode characters.
+     * Replace shortnames to emoji unicode characters.
      */
-    public static String shortnameToUnicode(String input, boolean removeIfUnsupported)
-    {
+    public static String shortnameToUnicode(String input, boolean removeIfUnsupported) {
         Matcher matcher = SHORTNAME_PATTERN.matcher(input);
-        boolean supported = Build.VERSION.SDK_INT >= 16;
+        boolean supported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
 
         while (matcher.find()) {
-            String unicode = _shortNameToUnicode.get(matcher.group(1));
+
+            String unicode = getUnicodeString(SHORT_NAME_TO_UNICODE.get(matcher.group(1)));
             if (unicode == null) {
                 continue;
             }
 
             if (supported) {
                 input = input.replace(":" + matcher.group(1) + ":", unicode);
-            } else if (!supported && removeIfUnsupported) {
+            } else if (removeIfUnsupported) {
                 input = input.replace(":" + matcher.group(1) + ":", "");
             }
         }
@@ -35,9 +43,66 @@ public abstract class Emojione
         return input;
     }
 
+    private static String getUnicodeString(Pair<Integer[], Integer> pair) {
+        if (pair != null) {
+            return new String(toPrimitive(pair.first), 0, pair.second);
+        }
+        return null;
+    }
+
+    private static int[] toPrimitive(Integer[] obj) {
+
+        int[] prim = new int[obj.length];
+        for (int i = 0; i < obj.length; i++) {
+            prim[i] = obj[i];
+        }
+        return prim;
+    }
+
+    /**
+     * Wrap emojis with font. Useful if the default font doesn't support (all) emojis.
+     *
+     * @param text containing emojis
+     * @param emojiTf ie. emojione
+     */
+    public static SpannableStringBuilder wrapEmojis(String text, Typeface emojiTf) {
+
+        SpannableStringBuilder res = new SpannableStringBuilder();
+        res.append(text);
+
+        for (int i = 0; i < text.length();) {
+            int start = i;
+            int end = i;
+            int unicode = Character.codePointAt(text, i);
+            int charCount = Character.charCount(unicode);
+
+            while (UNICODES.contains(unicode)) {
+                charCount = Character.charCount(unicode);
+                end += charCount;
+
+                i += charCount;
+                if (i >= text.length()) {
+                    break;
+                }
+                unicode = Character.codePointAt(text, i);
+            }
+
+            // Emoji sequence found
+            if (start != end) {
+                res.setSpan(new CustomTypeFaceSpan(emojiTf), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                i += charCount;
+            }
+        }
+        return res;
+    }
 
     static {
         <%= mapping %>
+
+        for (Pair<Integer[], Integer> val : SHORT_NAME_TO_UNICODE.values()) {
+            Collections.addAll(UNICODES, val.first);
+        }
     }
 
 }
